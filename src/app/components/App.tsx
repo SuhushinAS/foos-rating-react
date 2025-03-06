ec67f3095c034545bfb9b0fc231fe555a398e3c8
Alexander Suhushin <suhushinas@yandex.ru>
2025-03-06 14:55:52 +0700

    fix styles

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -2,12 +2,16 @@ import {appPath} from 'app/model/constants';
 import {store} from 'app/model/store';
 import {Layout} from 'modules/layout/components/Layout';
 import {Rating} from 'modules/rating/components/Rating';
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import 'styles/index.less';
 
 export const App = () => {
+  useLayoutEffect(() => {
+    document.body.classList.remove('opacity_hidden');
+  }, []);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
